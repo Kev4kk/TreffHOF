e55e268b097501a172cb100ebe204f6122fd1675
Kevin A <kevinakk2003@gmail.com>
2023-05-15 02:11:38 +0300

    klass vs keskmine

---
+++ aastateJärgi.py
@@ -0,0 +1,32 @@
+import json
+
+# Dictionary to store the summa values for each aasta
+summa_values = {}
+
+# Read the text file line by line
+with open('mentions.txt', 'r') as file:
+    for line in file:
+        # Parse each line as a JSON string
+        json_data = json.loads(line)
+
+        # Extract the aasta and summa values
+        aasta = json_data['aasta']
+        summa = json_data['summa']
+
+        # Store the summa value for the corresponding aasta
+        if aasta in summa_values:
+            summa_values[aasta].append(summa)
+        else:
+            summa_values[aasta] = [summa]
+
+# Calculate the average summa for each aasta
+average_values = {}
+for aasta, values in summa_values.items():
+    average_values[aasta] = sum(values) / len(values)
+
+# Sort the average values in descending order
+sorted_averages = sorted(average_values.items(), key=lambda x: x[1], reverse=True)
+
+# Print the average values in descending order
+for aasta, average in sorted_averages:
+    print(f"{aasta}: {average}")
